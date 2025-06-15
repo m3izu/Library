cb96266096b43bdf163e1d8faece621ace5e9b28
m3izu <rfsgalendez@gmail.com>
2025-06-15 23:21:09 +0800

    display library in dom

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const myLibrary = [];
+const myLibrary = [
+    new Books('The Hobbit', 'JRR Tolkien', 1937, 'Fantasy Childrens Fiction', false, crypto.randomUUID()),
+    new Books('The Alchemist', 'Paulo Coelho', 1988, 'Fantasy', true, crypto.randomUUID()),
+    new Books('A Tale of Two Cities', 'Charles Dickens', 1859, 'Historical Fiction', true, crypto.randomUUID()),
+];
 
 
 function Books(Name, Author, Year, Synopsis, Read, ID) {
@@ -11,14 +15,46 @@ function Books(Name, Author, Year, Synopsis, Read, ID) {
 }
 
 function addBooks() {
-    myLibrary.push(new Books(`${document.getElementById('name').value}`, 
-    `${document.getElementById('author').value}`, `${document.getElementById('year').value}`, 
-    `${document.getElementById('synopsis').value}`, `${false}`))
+    myLibrary.push(new Books(document.getElementById('name').value, 
+    document.getElementById('author').value, parseInt(document.getElementById('year').value), 
+    document.getElementById('synopsis').value, false, crypto.randomUUID()))
 }
 
 document.getElementById('form').addEventListener("submit", (e) => {
     e.preventDefault();
     addBooks();
     console.log(myLibrary);
+    document.getElementById('form').reset();
+    displayLibrary(myLibrary);
 }
 )
+
+function displayLibrary(bookArray) {
+    const container = document.getElementById('body');
+    container.innerHTML = '';
+
+    bookArray.forEach(element => {
+        const card = document.createElement('div');
+        card.classname = 'card';
+
+        const title = document.createElement('h2');
+        title.textContent = element.Name;
+
+        const author = document.createElement('p');
+        author.textContent = `Author: ${element.Author}`;
+        
+        const year = document.createElement('p');
+        year.textContent = `Year: ${element.Year}`;
+        
+        const synopsis = document.createElement('p');
+        synopsis.textContent = `Synopsis: ${element.Synopsis}`;
+
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(year);
+        card.appendChild(synopsis);
+        container.appendChild(card);
+    });
+}
+
+displayLibrary(myLibrary);
